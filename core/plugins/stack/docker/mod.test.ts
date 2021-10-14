ce0950948a4839265e6b82d0ee3ac1a0e05a87ef
joao10lima <joao.nascimento@instruct.com.br>
2021-10-14 17:14:09 -0300

    feat(core/cli) add docker release stage with sensible default
    Following the discussion https://github.com/pipelinit/pipelinit-cli/issues/34,
    this PR implements a release stack for docker images that can be built in the repository and
    with that adds a sensitive default to the Pipelinit configuration file with the possibility to other 'registries' for the generated CI.
    
    The template basically looks for paths with Dockerfiles that can be built and
    adds metadata involving the path found and the specified version.
    
    Resolves: https://github.com/pipelinit/pipelinit-cli/issues/80

--- core/plugins/stack/docker/mod.test.ts
+++ core/plugins/stack/docker/mod.test.ts
@@ -1,7 +1,12 @@
 import { context } from "../../../tests/mod.ts";
 import { assertEquals, deepMerge } from "../../../deps.ts";
+import { config, StackRegistry } from "../../../../cli/src/lib/config.ts";
+import { Platforms } from "../../../../cli/src/lib/platform.ts";
+import { FileEntry } from "../../../types.ts";
 
 import { introspector } from "./mod.ts";
+
+config.platforms = <Platforms> ["github"];
 
 const fakeContext = () => {
   return deepMerge(
@@ -15,6 +20,15 @@ const fakeContext = () => {
           }
           return false;
         },
+        each: async function* (glob: string): AsyncIterableIterator<FileEntry> {
+          if (glob === "**/Dockerfile") {
+            yield {
+              name: "Dockerfile",
+              path: "fake-path",
+            };
+          }
+          return;
+        },
       },
     },
   );
@@ -26,6 +40,34 @@ Deno.test("Plugins > Check if Dockerfile is identified", async () => {
   );
 
   assertEquals(result, {
+    dockerContext: {
+      paths: new Set(["fake-path"]),
+    },
     hasDockerImage: true,
+    registries: {
+      urls: [
+        "registry.hub.docker.com",
+      ],
+    },
+  });
+});
+
+Deno.test("Plugins > Check if Dockerfile is identified and Other registry", async () => {
+  config.registries = <StackRegistry> { docker: ["ghcr.io"] };
+
+  const result = await introspector.introspect(
+    fakeContext(),
+  );
+
+  assertEquals(result, {
+    dockerContext: {
+      paths: new Set(["fake-path"]),
+    },
+    hasDockerImage: true,
+    registries: {
+      urls: [
+        "ghcr.io",
+      ],
+    },
   });
 });
